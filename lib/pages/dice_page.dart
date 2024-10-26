fd4b399c3a18c6bffcffb8fd045c9596e14ac834
JesusRamirezGamarra <luciojesusramirezgamarra@gmail.com>
2024-10-26 12:51:50 -0500

    Primer commit - subir todos los archivos

---
+++ lib/pages/dice_page.dart
@@ -0,0 +1,92 @@
+ import 'package:flutter/material.dart';
+
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  String rutaImagen = "assets/images/dice1.png";
+  int dadoN = 1;
+  int n = 1;
+
+  void cambiarADado6(){
+    // print("Cambiando a dado 6");
+    setState(() {
+      rutaImagen = "assets/images/dice6.png";
+    });
+  }
+
+void cambiarADado(){
+    // print("Cambiando a dado 6");
+    dadoN = 6;
+    setState(() {
+      rutaImagen = "assets/images/dice6.png";
+    });
+  }
+
+
+void cambiarADadoN(int x){
+    // print("Cambiando a dado 6");
+    dadoN = n;
+    setState(() {
+      rutaImagen = "assets/images/dice$dadoN.png";
+    });
+    n= n+1;    
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black38,
+      appBar: AppBar(
+        title: Text("Dice Page"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                // // // Bordes circulares
+                // primera forma : 
+                // borderRadius: BorderRadius.circular(16),
+
+                // // // Segunda forma
+                borderRadius:BorderRadius.all(
+                  Radius.circular(16),
+                ),
+
+                //Tercera forma
+                // shape: BoxShape.circle,
+              ),
+              // color:Colors.red,
+              padding: EdgeInsets.all(16),
+              child: Image.asset(
+                // rutaImagen,
+                "assets/images/dice$dadoN.png",
+                // width: 200,
+                // height: 200,
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                print("Botón presionado");
+                // cambiarADado6();
+                //cambiarADado();
+                cambiarADadoN(n);
+              },
+              // child: Text("presioname"),
+              child: Text("presioname :: $n"),
+            ),            
+          ],
+        ),
+      ),
+    );
+  }
+}
